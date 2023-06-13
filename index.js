77911b07215e7545837a4ec7d69c6fbb01e62da8
Еж <easzyXD@gmail.com>
2023-06-13 22:41:05 +0300

    first commit

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const mongoose= require('mongoose');
 const express= require('express')
 const app=express()
 const cors=require("cors")
-app.use(express.json({extended: true}))
-app.use('/api/schedule', require('./routes/schedule'))
-app.use('/api/admin', require('./routes/admin'))
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT"]
 
 }))
+app.use(express.json({extended: true}))
+app.use('/api/schedule', require('./routes/schedule'))
+app.use('/api/admin', require('./routes/admin'))
 const start=async()=>{
     try{
         await mongoose.connect('mongodb+srv://admin:556677@cluster0.wkohydr.mongodb.net/?retryWrites=true&w=majority',{
